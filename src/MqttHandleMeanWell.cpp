fe864c0f332a9d238afabe0d712e678442d11211
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-01 19:15:38 +0100

    support Meanwell NPB charger

---
+++ src/MqttHandleMeanWell.cpp
@@ -0,0 +1,184 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2022 Thomas Basler and others
+ */
+#ifndef CHARGER_HUAWEI
+
+#include "MqttHandleMeanWell.h"
+#include "MeanWell_can.h"
+#include "MessageOutput.h"
+#include "MqttSettings.h"
+// #include "Failsafe.h"
+#include "WebApi_MeanWell.h"
+#include <ctime>
+
+static const char TAG[] = "[MeanWell MQTT]";
+
+MqttHandleMeanWellClass MqttHandleMeanWell;
+
+MqttHandleMeanWellClass::MqttHandleMeanWellClass()
+    : _loopTask(TASK_IMMEDIATE, TASK_FOREVER, std::bind(&MqttHandleMeanWellClass::loop, this))
+{
+}
+
+void MqttHandleMeanWellClass::init(Scheduler& scheduler)
+{
+    scheduler.addTask(_loopTask);
+    _loopTask.enable();
+
+    String const& prefix = MqttSettings.getPrefix();
+
+    auto subscribe = [&prefix, this](char const* subTopic, Topic t) {
+        String fullTopic(prefix + "meanwell/cmd/" + subTopic);
+        MqttSettings.subscribe(fullTopic.c_str(), 0,
+            std::bind(&MqttHandleMeanWellClass::onMqttMessage, this, t,
+                std::placeholders::_1, std::placeholders::_2,
+                std::placeholders::_3, std::placeholders::_4,
+                std::placeholders::_5, std::placeholders::_6));
+    };
+    subscribe("limit_voltage", Topic::LimitVoltage);
+    subscribe("limit_current", Topic::LimitCurrent);
+    subscribe("limit_curveCV", Topic::LimitCurveCV);
+    subscribe("limit_curveCC", Topic::LimitCurveCC);
+    subscribe("limit_curveFV", Topic::LimitCurveFV);
+    subscribe("limit_curveTC", Topic::LimitCurveTC);
+    subscribe("mode", Topic::Mode);
+
+    _lastPublish.set(Configuration.get().Mqtt.PublishInterval * 1000);
+}
+
+#define MQTTpublish(value)                                              \
+    if (!cMeanWell.UpdatesOnly || _last.value != MeanWellCan._rp.value) \
+        MqttSettings.publish(subtopic + #value, String(_last.value = MeanWellCan._rp.value));
+
+void MqttHandleMeanWellClass::loop()
+{
+    const CONFIG_T& config = Configuration.get();
+
+    std::unique_lock<std::mutex> mqttLock(_mqttMutex);
+
+    if (!config.MeanWell.Enabled) {
+        _mqttCallbacks.clear();
+        return;
+    }
+
+    for (auto& callback : _mqttCallbacks) {
+        callback();
+    }
+    _mqttCallbacks.clear();
+
+    mqttLock.unlock();
+
+    if (!MqttSettings.getConnected() || !_lastPublish.occured()) {
+        return;
+    }
+
+    const MeanWell_CONFIG_T& cMeanWell = Configuration.get().MeanWell;
+
+    const String subtopic = "meanwell/";
+    MqttSettings.publish(subtopic + "data_age", String((millis() - MeanWellCan.getLastUpdate()) / 1000));
+    if (!cMeanWell.UpdatesOnly || MeanWellCan._rp.CURVE_CONFIG.CUVE != _last.CURVE_CONFIG.CUVE)
+        MqttSettings.publish(subtopic + "cuve", String(_last.CURVE_CONFIG.CUVE = MeanWellCan._rp.CURVE_CONFIG.CUVE));
+    if (!cMeanWell.UpdatesOnly || MeanWellCan._rp.CURVE_CONFIG.STGS != _last.CURVE_CONFIG.STGS)
+        MqttSettings.publish(subtopic + "stgs", String(_last.CURVE_CONFIG.STGS = MeanWellCan._rp.CURVE_CONFIG.STGS));
+    MQTTpublish(operation);
+    MQTTpublish(inputVoltage);
+    MQTTpublish(outputVoltage);
+    MQTTpublish(outputCurrent);
+    MQTTpublish(outputPower);
+    MQTTpublish(outputVoltageSet);
+    MQTTpublish(outputCurrentSet);
+    MQTTpublish(curveCV);
+    MQTTpublish(curveCC);
+    MQTTpublish(curveFV);
+    MQTTpublish(curveTC);
+    MQTTpublish(internalTemperature);
+    MQTTpublish(efficiency);
+
+    _lastPublish.set(Configuration.get().Mqtt.PublishInterval * 1000);
+    yield();
+}
+
+void MqttHandleMeanWellClass::onMqttMessage(Topic t,
+    const espMqttClientTypes::MessageProperties& properties,
+    const char* topic, const uint8_t* payload, size_t len,
+    size_t index, size_t total)
+{
+    std::string strValue(reinterpret_cast<const char*>(payload), len);
+    float payload_val = -1;
+    try {
+        payload_val = std::stof(strValue);
+    } catch (std::invalid_argument const& e) {
+        MessageOutput.printf("%s handler: cannot parse payload of topic '%s' as float: %s\r\n", TAG, topic, strValue.c_str());
+        return;
+    }
+
+    std::lock_guard<std::mutex> mqttLock(_mqttMutex);
+
+    switch (t) {
+    case Topic::LimitVoltage:
+        if (MqttSettings.getVerboseLogging())
+            MessageOutput.printf("%s Limit Voltage: %.2f V\r\n", TAG, payload_val);
+        _mqttCallbacks.push_back(std::bind(&MeanWellCanClass::setValue,
+            &MeanWellCan, payload_val, MEANWELL_SET_VOLTAGE));
+        break;
+    case Topic::LimitCurrent:
+        if (MqttSettings.getVerboseLogging())
+            MessageOutput.printf("%s Limit Current: %.2f V\r\n", TAG, payload_val);
+        _mqttCallbacks.push_back(std::bind(&MeanWellCanClass::setValue,
+            &MeanWellCan, payload_val, MEANWELL_SET_CURRENT));
+        break;
+    case Topic::LimitCurveCV:
+        if (MqttSettings.getVerboseLogging())
+            MessageOutput.printf("%s Limit Curve CV: %.2f V\r\n", TAG, payload_val);
+        _mqttCallbacks.push_back(std::bind(&MeanWellCanClass::setValue,
+            &MeanWellCan, payload_val, MEANWELL_SET_CURVE_CV));
+        break;
+    case Topic::LimitCurveCC:
+        if (MqttSettings.getVerboseLogging())
+            MessageOutput.printf("%s Limit Curve CC: %.2f V\r\n", TAG, payload_val);
+        _mqttCallbacks.push_back(std::bind(&MeanWellCanClass::setValue,
+            &MeanWellCan, payload_val, MEANWELL_SET_CURVE_CC));
+        break;
+    case Topic::LimitCurveFV:
+        if (MqttSettings.getVerboseLogging())
+            MessageOutput.printf("%s Limit Curve FV: %.2f V\r\n", TAG, payload_val);
+        _mqttCallbacks.push_back(std::bind(&MeanWellCanClass::setValue,
+            &MeanWellCan, payload_val, MEANWELL_SET_CURVE_FV));
+        break;
+    case Topic::LimitCurveTC:
+        if (MqttSettings.getVerboseLogging())
+            MessageOutput.printf("%s Limit Curve TC: %.2f V\r\n", TAG, payload_val);
+        _mqttCallbacks.push_back(std::bind(&MeanWellCanClass::setValue,
+            &MeanWellCan, payload_val, MEANWELL_SET_CURVE_TC));
+        break;
+    case Topic::Mode:
+        // Control power on/off
+        if (MqttSettings.getVerboseLogging())
+            MessageOutput.printf("%s Power Mode: %s\r\n", TAG, payload_val == 0 ? "off" : payload_val == 1 ? "on"
+                    : payload_val == 2                                                                     ? "auto"
+                                                                                                           : "unknown");
+        switch (static_cast<int>(payload_val)) {
+        case 1:
+            _mqttCallbacks.push_back(std::bind(&MeanWellCanClass::setAutomaticChargeMode,
+                &MeanWellCan, false));
+            _mqttCallbacks.push_back(std::bind(&MeanWellCanClass::setPower,
+                &MeanWellCan, true));
+            break;
+        case 2:
+            _mqttCallbacks.push_back(std::bind(&MeanWellCanClass::setAutomaticChargeMode,
+                &MeanWellCan, true));
+            break;
+        case 0:
+            _mqttCallbacks.push_back(std::bind(&MeanWellCanClass::setAutomaticChargeMode,
+                &MeanWellCan, false));
+            break;
+        default:
+            MessageOutput.printf("%s Invalid mode %.0f\r\n", TAG, payload_val);
+            break;
+        }
+        break;
+    }
+}
+
+#endif
