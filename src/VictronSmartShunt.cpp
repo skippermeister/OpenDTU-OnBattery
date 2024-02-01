222514d445c606e2b225c763f0c57c5b343e7dd0
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-02 00:25:58 +0100

    Update VictronSmartShunt.cpp

--- src/VictronSmartShunt.cpp
+++ src/VictronSmartShunt.cpp
@@ -1,27 +1,27 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
+#ifdef USE_VICTRON_SMART_SHUNT
+
 #include "VictronSmartShunt.h"
 #include "Configuration.h"
 #include "PinMapping.h"
 #include "MessageOutput.h"
 
-
-bool VictronSmartShunt::init(bool verboseLogging)
+bool VictronSmartShunt::init()
 {
-    MessageOutput.println("[VictronSmartShunt] Initialize interface...");
+    MessageOutput.println("Initialize Ve.Direct interface...");
 
     const PinMapping_t& pin = PinMapping.get();
-    MessageOutput.printf("[VictronSmartShunt] Interface rx = %d, tx = %d\r\n",
-            pin.battery_rx, pin.battery_tx);
+    MessageOutput.printf(" rx = %d, tx = %d\r\n", pin.battery_rx, pin.battery_tx);
 
     if (pin.battery_rx < 0) {
-        MessageOutput.println("[VictronSmartShunt] Invalid pin config");
+        MessageOutput.println(" Invalid pin config");
         return false;
     }
 
     auto tx = static_cast<gpio_num_t>(pin.battery_tx);
     auto rx = static_cast<gpio_num_t>(pin.battery_rx);
 
-    VeDirectShunt.init(rx, tx, &MessageOutput, verboseLogging);
+    VeDirectShunt.init(rx, tx, &MessageOutput, VictronMppt.getVerboseLogging());
     return true;
 }
 
@@ -34,3 +34,5 @@ void VictronSmartShunt::loop()
     _stats->updateFrom(VeDirectShunt.veFrame);
     _lastUpdate = VeDirectShunt.getLastUpdate();
 }
+
+#endif
