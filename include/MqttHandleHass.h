bee3a60ba55affa40b8dfe4f1845ea33a8eaedba
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-01 22:41:15 +0100

    add conditional compilation
    enable/disable HASS

--- include/MqttHandleHass.h
+++ include/MqttHandleHass.h
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
+#ifdef USE_HASS
+
 #pragma once
 
 #include <ArduinoJson.h>
@@ -81,3 +83,5 @@ private:
 };
 
 extern MqttHandleHassClass MqttHandleHass;
+
+#endif
