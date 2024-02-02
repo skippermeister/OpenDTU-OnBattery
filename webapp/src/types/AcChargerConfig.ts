6c5663d3dfe3483ce35aecde3dcd168aafdd155b
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-02 23:28:38 +0100

    Add files via upload

---
+++ webapp/src/types/AcChargerConfig.ts
@@ -0,0 +1,12 @@
+export interface AcChargerConfig {
+  enabled: boolean;
+  can_controller_frequency: number;
+  pollinterval: number;
+  updatesonly: boolean;
+  min_voltage: number;
+  max_voltage: number;
+  min_current: number;
+  max_current: number;
+  hysteresis: number;
+  verbose_logging: boolean;
+}
