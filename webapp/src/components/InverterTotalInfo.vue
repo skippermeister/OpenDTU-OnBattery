d3e44df478b0d4944850e628d997a56e4458fc4b
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-02 23:08:51 +0100

    Update InverterTotalInfo.vue

--- webapp/src/components/InverterTotalInfo.vue
+++ webapp/src/components/InverterTotalInfo.vue
@@ -1,42 +1,80 @@
 <template>
-  <div v-show="totalVeData.enabled">
-    <div class="row row-cols-1 row-cols-md-3 g-3">
-        <div class="col">
-            <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.MpptTotalYieldTotal')">
-                <h2>
-                    {{ $n(totalVeData.total.YieldTotal.v, 'decimal', {
-                        minimumFractionDigits: totalVeData.total.YieldTotal.d,
-                        maximumFractionDigits:totalVeData.total.YieldTotal.d
-                    }) }}
-                    <small class="text-muted">{{ totalVeData.total.YieldTotal.u }}</small>
-                </h2>
-            </CardElement>
-        </div>
-        <div class="col">
-            <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.MpptTotalYieldDay')">
-                <h2>
-                    {{ $n(totalVeData.total.YieldDay.v, 'decimal', {
-                        minimumFractionDigits: totalVeData.total.YieldDay.d,
-                        maximumFractionDigits: totalVeData.total.YieldDay.d
-                    }) }}
-                    <small class="text-muted">{{ totalVeData.total.YieldDay.u }}</small>
-                </h2>
-            </CardElement>
-        </div>
-        <div class="col">
-            <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.MpptTotalPower')">
-                <h2>
-                    {{ $n(totalVeData.total.Power.v, 'decimal', {
-                        minimumFractionDigits: totalVeData.total.Power.d,
-                        maximumFractionDigits: totalVeData.total.Power.d
-                    }) }}
-                    <small class="text-muted">{{ totalVeData.total.Power.u }}</small>
-                </h2>
-            </CardElement>
+    <div v-show="totalREFUsolData.enabled">
+        <div class="row row-cols-1 row-cols-md-3 g-3">
+            <div class="col">
+                <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.REFUsolTotalDeliveredTotal')">
+                    <h2>
+                        {{ $n(totalREFUsolData.total.YieldTotal.v, 'decimal', {
+                            minimumFractionDigits: totalREFUsolData.total.YieldTotal.d,
+                            maximumFractionDigits: totalREFUsolData.total.YieldTotal.d
+                        }) }}
+                        <small class="text-muted">{{ totalREFUsolData.total.YieldTotal.u }}</small>
+                    </h2>
+                </CardElement>
+            </div>
+            <div class="col">
+                <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.REFUsolTotalDeliveredDay')">
+                    <h2>
+                        {{ $n(totalREFUsolData.total.YieldDay.v, 'decimal', {
+                            minimumFractionDigits: totalREFUsolData.total.YieldDay.d,
+                            maximumFractionDigits: totalREFUsolData.total.YieldDay.d
+                        }) }}
+                      <small class="text-muted">{{ totalREFUsolData.total.YieldDay.u }}</small>
+                    </h2>
+                </CardElement>
+            </div>
+            <div class="col">
+                <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.REFUsolTotalPower')">
+                    <h2>
+                        {{ $n(totalREFUsolData.total.Power.v, 'decimal', {
+                            minimumFractionDigits: totalREFUsolData.total.Power.d,
+                            maximumFractionDigits: totalREFUsolData.total.Power.d
+                        }) }}
+                        <small class="text-muted">{{ totalREFUsolData.total.Power.u }}</small>
+                    </h2>
+                </CardElement>
+            </div>
         </div>
     </div>
-  </div>
-  <div class="row row-cols-1 row-cols-md-3 g-3">
+    <div v-show="totalVeData.enabled">
+        <div class="row row-cols-1 row-cols-md-3 g-3">
+            <div class="col">
+                <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.MpptTotalYieldTotal')">
+                    <h2>
+                        {{ $n(totalVeData.total.YieldTotal.v, 'decimal', {
+                            minimumFractionDigits: totalVeData.total.YieldTotal.d,
+                            maximumFractionDigits: totalVeData.total.YieldTotal.d
+                        }) }}
+                        <small class="text-muted">{{ totalVeData.total.YieldTotal.u }}</small>
+                    </h2>
+                </CardElement>
+            </div>
+            <div class="col">
+                <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.MpptTotalYieldDay')">
+                    <h2>
+                        {{ $n(totalVeData.total.YieldDay.v, 'decimal', {
+                            minimumFractionDigits: totalVeData.total.YieldDay.d,
+                            maximumFractionDigits: totalVeData.total.YieldDay.d
+                        }) }}
+                        <small class="text-muted">{{ totalVeData.total.YieldDay.u }}</small>
+                    </h2>
+                </CardElement>
+            </div>
+            <div class="col">
+                <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.MpptTotalPower')">
+                    <h2>
+                        {{ $n(totalVeData.total.Power.v, 'decimal', {
+                            minimumFractionDigits: totalVeData.total.Power.d,
+                            maximumFractionDigits: totalVeData.total.Power.d
+                        }) }}
+                        <small class="text-muted">{{ totalVeData.total.Power.u }}</small>
+                    </h2>
+                </CardElement>
+            </div>
+        </div>
+    </div>
+
+    <div class="row row-cols-1 row-cols-md-3 g-3">
         <div class="col">
             <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.InverterTotalYieldTotal')">
                 <h2>
@@ -70,48 +108,65 @@
                 </h2>
             </CardElement>
         </div>
-  </div>
-  <div v-show="totalBattData.enabled || powerMeterData.enabled || huaweiData.enabled">
-    <div class="row row-cols-1 row-cols-md-3 g-3">
-      <div class="col" v-show="totalBattData.enabled">
-            <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.BatterySoc')">
-                <h2>
-                    {{ $n(totalBattData.soc.v, 'decimal', {
-                        minimumFractionDigits: totalBattData.soc.d,
-                        maximumFractionDigits: totalBattData.soc.d
-                    }) }}
-                    <small class="text-muted">{{ totalBattData.soc.u }}</small>
-                </h2>
-            </CardElement>
-      </div>
-      <div class="col" v-show="powerMeterData.enabled">
-            <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.HomePower')">
-                <h2>
-                    {{ $n(powerMeterData.Power.v, 'decimal', {
-                        minimumFractionDigits: powerMeterData.Power.d,
-                        maximumFractionDigits: powerMeterData.Power.d
-                    }) }}
-                    <small class="text-muted">{{powerMeterData.Power.u }}</small>
-                </h2>
-            </CardElement>
-      </div>
-      <div class="col" v-show="huaweiData.enabled">
-            <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.HuaweiPower')">
-                <h2>
-                    {{ $n(huaweiData.Power.v, 'decimal', {
-                        minimumFractionDigits: huaweiData.Power.d,
-                        maximumFractionDigits: huaweiData.Power.d
-                    }) }}
-                    <small class="text-muted">{{huaweiData.Power.u }}</small>
-                </h2>
-            </CardElement>
-      </div>
     </div>
-  </div>
+
+    <div v-show="powerMeterData.enabled || meanwellData.enabled">
+        <div class="row row-cols-1 row-cols-md-3 g-3">
+            <div v-show="powerMeterData.enabled" class="col">
+                <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.GridPower')">
+                    <h2>
+                        {{ $n(powerMeterData.GridPower.v, 'decimal', {
+                            minimumFractionDigits: powerMeterData.GridPower.d,
+                            maximumFractionDigits: powerMeterData.GridPower.d
+                        }) }}
+                        <small class="text-muted">{{ powerMeterData.GridPower.u }}</small>
+                    </h2>
+                </CardElement>
+            </div>
+            <div v-show="powerMeterData.enabled" class="col">
+                <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.HousePower')">
+                    <h2>
+                        {{ $n(powerMeterData.HousePower.v, 'decimal', {
+                            minimumFractionDigits: powerMeterData.HousePower.d,
+                            maximumFractionDigits: powerMeterData.HousePower.d
+                        }) }}
+                        <small class="text-muted">{{ powerMeterData.HousePower.u }}</small>
+                    </h2>
+                </CardElement>
+            </div>
+            <div v-show="meanwellData.enabled" class="col">
+                <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.MeanWellPower')">
+                    <h2>
+                        {{ $n(meanwellData.Power.v, 'decimal', {
+                            minimumFractionDigits: meanwellData.Power.d,
+                            maximumFractionDigits: meanwellData.Power.d
+                        }) }}
+                        <small class="text-muted">{{ meanwellData.Power.u }}</small>
+                    </h2>
+                </CardElement>
+            </div>
+        </div>
+    </div>
+
+    <div v-show="totalBattData.enabled">
+        <div class="row row-cols-1 row-cols-md-3 g-3">
+            <div class="col">
+                <CardElement centerContent textVariant="text-bg-success" :text="$t('invertertotalinfo.BatterySoc')">
+                    <h2>
+                        {{ $n(totalBattData.soc.v, 'decimal', {
+                            minimumFractionDigits: totalBattData.soc.d,
+                            maximumFractionDigits: totalBattData.soc.d
+                        }) }}
+                        <small class="text-muted">{{ totalBattData.soc.u }}</small>
+                    </h2>
+                </CardElement>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-import type { Battery, Total, Vedirect, Huawei, PowerMeter } from '@/types/LiveDataStatus';
+import type { Battery, Total, Vedirect, REFUsol, MeanWell, PowerMeter } from '@/types/LiveDataStatus';
 import CardElement from './CardElement.vue';
 import { defineComponent, type PropType } from 'vue';
 
@@ -119,12 +174,13 @@ export default defineComponent({
     components: {
         CardElement,
     },
-  props: {
-    totalData: { type: Object as PropType<Total>, required: true },
-    totalVeData: { type: Object as PropType<Vedirect>, required: true },
-    totalBattData: { type: Object as PropType<Battery>, required: true },
-    powerMeterData: { type: Object as PropType<Huawei>, required: true },
-    huaweiData: { type: Object as PropType<PowerMeter>, required: true },
-  },
+    props: {
+        totalData: { type: Object as PropType<Total>, required: true },
+        totalVeData: { type: Object as PropType<Vedirect>, required: true },
+        totalREFUsolData: { type: Object as PropType<REFUsol>, required: true },
+        totalBattData: { type: Object as PropType<Battery>, required: true },
+        powerMeterData: { type: Object as PropType<PowerMeter>, required: true },
+        meanwellData: { type: Object as PropType<MeanWell>, required: true },
+    },
 });
 </script>
