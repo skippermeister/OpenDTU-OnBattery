79c8a26b368b14917b34d86db3030706eb5794f5
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-02 22:27:02 +0100

    Update PylontechCanReceiver.h

--- include/PylontechCanReceiver.h
+++ include/PylontechCanReceiver.h
@@ -1,12 +1,22 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
+#ifdef USE_PYLONTECH_CAN_RECEIVER
+
 #pragma once
 
-#include "Configuration.h"
 #include "Battery.h"
-#include <espMqttClient.h>
-#include <driver/twai.h>
+#include "Configuration.h"
 #include <Arduino.h>
+#include <driver/twai.h>
+#include <espMqttClient.h>
 #include <memory>
+
+#ifndef PYLONTECH_PIN_RX
+#define PYLONTECH_PIN_RX 27
+#endif
+
+#ifndef PYLONTECH_PIN_TX
+#define PYLONTECH_PIN_TX 26
+#endif
 
 class PylontechCanReceiver : public BatteryProvider {
 public:
@@ -16,14 +26,15 @@ public:
     std::shared_ptr<BatteryStats> getStats() const final { return _stats; }
 
 private:
-    uint16_t readUnsignedInt16(uint8_t *data);
-    int16_t readSignedInt16(uint8_t *data);
-    float scaleValue(int16_t value, float factor);
-    bool getBit(uint8_t value, uint8_t bit);
+    uint16_t readUnsignedInt16(uint8_t* data) { return ((*(data + 1)) << 8) + *data; };
+    int16_t readSignedInt16(uint8_t* data) { return this->readUnsignedInt16(data); };
+    //    void readString(char* str, uint8_t numBytes);
+    //    void readBooleanBits8(bool* b, uint8_t numBits);
+    float scaleValue(int16_t value, float factor) { return value * factor; };
+    bool getBit(uint8_t value, uint8_t bit) { return (value & (1 << bit)) >> bit; };
 
     void dummyData();
 
-    bool _verboseLogging = true;
-    std::shared_ptr<PylontechBatteryStats> _stats =
-        std::make_shared<PylontechBatteryStats>();
+    std::shared_ptr<PylontechCanBatteryStats> _stats = std::make_shared<PylontechCanBatteryStats>();
 };
+#endif
