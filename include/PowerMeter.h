826d35b14882404ee7793c3ed9b3a82348943032
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-02 22:26:20 +0100

    Update PowerMeter.h

--- include/PowerMeter.h
+++ include/PowerMeter.h
@@ -2,13 +2,14 @@
 #pragma once
 
 #include "Configuration.h"
-#include <espMqttClient.h>
-#include <Arduino.h>
-#include <map>
-#include <list>
 #include "SDM.h"
 #include "sml.h"
+#include <Arduino.h>
 #include <TaskSchedulerDeclarations.h>
+#include <TimeoutHelper.h>
+#include <espMqttClient.h>
+#include <list>
+#include <map>
 
 #ifndef SDM_RX_PIN
 #define SDM_RX_PIN 13
@@ -23,10 +24,26 @@
 #endif
 
 typedef struct {
-  const unsigned char OBIS[6];
-  void (*Fn)(double&);
-  float* Arg;
+    const unsigned char OBIS[6];
+    void (*Fn)(double&);
+    float* Arg;
 } OBISHandler;
+
+typedef struct {
+    float Power1;
+    float Power2;
+    float Power3;
+    /*
+        float Voltage1;
+        float Voltage2;
+        float Voltage3;
+        float Import;
+        float Export;
+    */
+    float PowerTotal;
+    float HousePower;
+
+} PowerMeter_t;
 
 class PowerMeterClass {
 public:
@@ -37,43 +54,43 @@ public:
         SOURCE_HTTP = 3,
         SOURCE_SML = 4
     };
+    PowerMeterClass();
     void init(Scheduler& scheduler);
     float getPowerTotal(bool forceUpdate = true);
-    uint32_t getLastPowerMeterUpdate();
+    void setHousePower(float value);
+    float getHousePower(void);
+    uint32_t getLastPowerMeterUpdate(void) { return _lastPowerMeterUpdate; }
+
+    bool getVerboseLogging(void) { return _verboseLogging; };
+    void setVerboseLogging(bool logging) { _verboseLogging = logging; };
 
 private:
     void loop();
+
+    Task _loopTask;
+
     void mqtt();
+    void onMqttMessage(const espMqttClientTypes::MessageProperties& properties, const char* topic, const uint8_t* payload, size_t len, size_t index, size_t total);
 
-    void onMqttMessage(const espMqttClientTypes::MessageProperties& properties,
-        const char* topic, const uint8_t* payload, size_t len, size_t index, size_t total);
-
-     Task _loopTask;
-
-    bool _verboseLogging = true;
-    uint32_t _lastPowerMeterCheck;
+    TimeoutHelper _lastPowerMeterCheck;
     // Used in Power limiter for safety check
     uint32_t _lastPowerMeterUpdate;
 
-    float _powerMeter1Power = 0.0;
-    float _powerMeter2Power = 0.0;
-    float _powerMeter3Power = 0.0;
-    float _powerMeter1Voltage = 0.0;
-    float _powerMeter2Voltage = 0.0;
-    float _powerMeter3Voltage = 0.0;
-    float _powerMeterImport = 0.0;
-    float _powerMeterExport = 0.0;
+    PowerMeter_t _powerMeter = { 0.0, 0.0, 0.0, 0.0, 0.0 }; //, 0.0, 0.0, 0.0, 0.0, 0.0};
+    PowerMeter_t _lastPowerMeter = { 0.0, 0.0, 0.0, 0.0, 0.0 }; //, 0.0, 0.0, 0.0, 0.0, 0.0};
 
     std::map<String, float*> _mqttSubscriptions;
 
     void readPowerMeter();
 
     bool smlReadLoop();
-    const std::list<OBISHandler> smlHandlerList{
-        {{0x01, 0x00, 0x10, 0x07, 0x00, 0xff}, &smlOBISW, &_powerMeter1Power},
-        {{0x01, 0x00, 0x01, 0x08, 0x00, 0xff}, &smlOBISWh, &_powerMeterImport},
-        {{0x01, 0x00, 0x02, 0x08, 0x00, 0xff}, &smlOBISWh, &_powerMeterExport}
+    const std::list<OBISHandler> smlHandlerList {
+        { { 0x01, 0x00, 0x10, 0x07, 0x00, 0xff }, &smlOBISW, &_powerMeter.Power1 }
+        //        {{0x01, 0x00, 0x01, 0x08, 0x00, 0xff}, &smlOBISWh, &_powerMeter.Import},
+        //        {{0x01, 0x00, 0x02, 0x08, 0x00, 0xff}, &smlOBISWh, &_powerMeter.Export}
     };
+
+    bool _verboseLogging = false;
 };
 
 extern PowerMeterClass PowerMeter;
