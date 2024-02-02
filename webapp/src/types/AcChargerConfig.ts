2f614dca122ded58f05ed072d4562b57edbb7c46
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-02 23:27:59 +0100

    Delete webapp/src/types/AcChargerConfig.ts

--- webapp/src/types/AcChargerConfig.ts
+++
@@ -1,9 +0,0 @@
-export interface AcChargerConfig {
-  enabled: boolean;
-  can_controller_frequency: number;
-  auto_power_enabled: boolean;
-  voltage_limit: number;
-  enable_voltage_limit: number;
-  lower_power_limit: number;
-  upper_power_limit: number;
-}
