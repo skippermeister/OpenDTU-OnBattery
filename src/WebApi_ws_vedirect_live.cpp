731280d899c964e9f44f11be3acd82c2f00f10ee
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-02 02:32:19 +0100

    Update WebApi_ws_vedirect_live.cpp

--- src/WebApi_ws_vedirect_live.cpp
+++ src/WebApi_ws_vedirect_live.cpp
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Copyright (C) 2022-2024 Thomas Basler and others
+ * Copyright (C) 2022 Thomas Basler and others
  */
 #include "WebApi_ws_vedirect_live.h"
 #include "AsyncJson.h"
 #include "Configuration.h"
 #include "MessageOutput.h"
+#include "PowerLimiter.h"
 #include "Utils.h"
+#include "VictronMppt.h"
 #include "WebApi.h"
 #include "defaults.h"
-#include "PowerLimiter.h"
-#include "VictronMppt.h"
 
 WebApiWsVedirectLiveClass::WebApiWsVedirectLiveClass()
     : _ws("/vedirectlivedata")
+    , _wsCleanupTask(1 * TASK_SECOND, TASK_FOREVER, std::bind(&WebApiWsVedirectLiveClass::wsCleanupTaskCb, this))
+    , _sendDataTask(500 * TASK_MILLISECOND, TASK_FOREVER, std::bind(&WebApiWsVedirectLiveClass::sendDataTaskCb, this))
 {
 }
 
@@ -32,17 +34,12 @@ void WebApiWsVedirectLiveClass::init(AsyncWebServer& server, Scheduler& schedule
     _server->addHandler(&_ws);
     _ws.onEvent(std::bind(&WebApiWsVedirectLiveClass::onWebsocketEvent, this, _1, _2, _3, _4, _5, _6));
 
-    
+    _lastWsPublish.set(10 * 1000);
+
     scheduler.addTask(_wsCleanupTask);
-    _wsCleanupTask.setCallback(std::bind(&WebApiWsVedirectLiveClass::wsCleanupTaskCb, this));
-    _wsCleanupTask.setIterations(TASK_FOREVER);
-    _wsCleanupTask.setInterval(1 * TASK_SECOND);
     _wsCleanupTask.enable();
 
     scheduler.addTask(_sendDataTask);
-    _sendDataTask.setCallback(std::bind(&WebApiWsVedirectLiveClass::sendDataTaskCb, this));
-    _sendDataTask.setIterations(TASK_FOREVER);
-    _sendDataTask.setInterval(500 * TASK_MILLISECOND);
     _sendDataTask.enable();
 }
 
@@ -50,6 +47,12 @@ void WebApiWsVedirectLiveClass::wsCleanupTaskCb()
 {
     // see: https://github.com/me-no-dev/ESPAsyncWebServer#limiting-the-number-of-web-socket-clients
     _ws.cleanupClients();
+
+    if (Configuration.get().Security.AllowReadonly) {
+        _ws.setAuthentication("", "");
+    } else {
+        _ws.setAuthentication(AUTH_USERNAME, Configuration.get().Security.Password);
+    }
 }
 
 void WebApiWsVedirectLiveClass::sendDataTaskCb()
@@ -66,35 +69,50 @@ void WebApiWsVedirectLiveClass::sendDataTaskCb()
     _dataAgeMillis = VictronMppt.getDataAgeMillis();
 
     // Update on ve.direct change or at least after 10 seconds
-    if (millis() - _lastWsPublish > (10 * 1000) || lastDataAgeMillis > _dataAgeMillis) {
-        
-        try {
-            std::lock_guard<std::mutex> lock(_mutex);
-            DynamicJsonDocument root(_responseSize);
-            if (Utils::checkJsonAlloc(root, __FUNCTION__, __LINE__)) {
-                JsonVariant var = root;
-                generateJsonResponse(var);
+    if (!_lastWsPublish.occured() && (lastDataAgeMillis <= _dataAgeMillis)) {
+        return;
+    }
 
-                String buffer;
-                serializeJson(root, buffer);
-           
-                if (Configuration.get().Security.AllowReadonly) {
-                    _ws.setAuthentication("", "");
-                } else {
-                    _ws.setAuthentication(AUTH_USERNAME, Configuration.get().Security.Password);
-                }
+    try {
+        std::lock_guard<std::mutex> lock(_mutex);
+        DynamicJsonDocument root(_responseSize);
+        if (Utils::checkJsonAlloc(root, __FUNCTION__, __LINE__)) {
+            JsonVariant var = root;
+            generateJsonResponse(var);
 
-                _ws.textAll(buffer);
-            }
+            String buffer;
+            serializeJson(root, buffer);
 
-        } catch (std::bad_alloc& bad_alloc) {
-            MessageOutput.printf("Calling /api/vedirectlivedata/status has temporarily run out of resources. Reason: \"%s\".\r\n", bad_alloc.what());
-        } catch (const std::exception& exc) {
-            MessageOutput.printf("Unknown exception in /api/vedirectlivedata/status. Reason: \"%s\".\r\n", exc.what());
+            _ws.textAll(buffer);
         }
 
-        _lastWsPublish = millis();
+    } catch (std::bad_alloc& bad_alloc) {
+        MessageOutput.printf("Calling /api/vedirectlivedata/status has temporarily run out of resources. Reason: \"%s\".\r\n", bad_alloc.what());
+    } catch (const std::exception& exc) {
+        MessageOutput.printf("Unknown exception in /api/vedirectlivedata/status. Reason: \"%s\".\r\n", exc.what());
     }
+
+    _lastWsPublish.reset();
+}
+
+template <typename T>
+void addOutputValue(JsonVariant& root, std::string const& name,
+    T&& value, std::string const& unit, uint8_t precision)
+{
+    auto jsonValue = root["output"][name];
+    jsonValue["v"] = value;
+    jsonValue["u"] = unit;
+    jsonValue["d"] = precision;
+}
+
+template <typename T>
+void addInputValue(JsonVariant& root, std::string const& name,
+    T&& value, std::string const& unit, uint8_t precision)
+{
+    auto jsonValue = root["input"][name];
+    jsonValue["v"] = value;
+    jsonValue["u"] = unit;
+    jsonValue["d"] = precision;
 }
 
 void WebApiWsVedirectLiveClass::generateJsonResponse(JsonVariant& root)
@@ -113,67 +131,35 @@ void WebApiWsVedirectLiveClass::generateJsonResponse(JsonVariant& root)
     root["device"]["OR"] = spMpptData->getOrAsString();
     root["device"]["MPPT"] = spMpptData->getMpptAsString();
     root["device"]["HSDS"]["v"] = spMpptData->HSDS;
-    root["device"]["HSDS"]["u"] = "d";
+    root["device"]["HSDS"]["u"] = "Days";
 
-    // battery info    
-    root["output"]["P"]["v"] = spMpptData->P;
-    root["output"]["P"]["u"] = "W";
-    root["output"]["P"]["d"] = 0;
-    root["output"]["V"]["v"] = spMpptData->V;
-    root["output"]["V"]["u"] = "V";
-    root["output"]["V"]["d"] = 2;
-    root["output"]["I"]["v"] = spMpptData->I;
-    root["output"]["I"]["u"] = "A";
-    root["output"]["I"]["d"] = 2;
-    root["output"]["E"]["v"] = spMpptData->E;
-    root["output"]["E"]["u"] = "%";
-    root["output"]["E"]["d"] = 1;
+    // battery info
+    addOutputValue(root, "P", spMpptData->P, "W", 0);
+    addOutputValue(root, "V", spMpptData->V, "V", 2);
+    addOutputValue(root, "I", spMpptData->I, "A", 2);
+    addOutputValue(root, "E", spMpptData->E, "%", 1);
 
     // panel info
-    root["input"]["PPV"]["v"] = spMpptData->PPV;
-    root["input"]["PPV"]["u"] = "W";
-    root["input"]["PPV"]["d"] = 0;
-    root["input"]["VPV"]["v"] = spMpptData->VPV;
-    root["input"]["VPV"]["u"] = "V";
-    root["input"]["VPV"]["d"] = 2;
-    root["input"]["IPV"]["v"] = spMpptData->IPV;
-    root["input"]["IPV"]["u"] = "A";
-    root["input"]["IPV"]["d"] = 2;
-    root["input"]["YieldToday"]["v"] = spMpptData->H20;
-    root["input"]["YieldToday"]["u"] = "kWh";
-    root["input"]["YieldToday"]["d"] = 3;
-    root["input"]["YieldYesterday"]["v"] = spMpptData->H22;
-    root["input"]["YieldYesterday"]["u"] = "kWh";
-    root["input"]["YieldYesterday"]["d"] = 3;
-    root["input"]["YieldTotal"]["v"] = spMpptData->H19;
-    root["input"]["YieldTotal"]["u"] = "kWh";
-    root["input"]["YieldTotal"]["d"] = 3;
-    root["input"]["MaximumPowerToday"]["v"] = spMpptData->H21;
-    root["input"]["MaximumPowerToday"]["u"] = "W";
-    root["input"]["MaximumPowerToday"]["d"] = 0;
-    root["input"]["MaximumPowerYesterday"]["v"] = spMpptData->H23;
-    root["input"]["MaximumPowerYesterday"]["u"] = "W";
-    root["input"]["MaximumPowerYesterday"]["d"] = 0;
+    addInputValue(root, "PPV", spMpptData->PPV, "W", 0);
+    addInputValue(root, "VPV", spMpptData->VPV, "V", 2);
+    addInputValue(root, "IPV", spMpptData->IPV, "A", 2);
+    addInputValue(root, "YieldToday", spMpptData->H20, "kWh", 3);
+    addInputValue(root, "YieldYesterday", spMpptData->H22, "kWh", 3);
+    addInputValue(root, "YieldTotal", spMpptData->H19, "kWh", 3);
+    addInputValue(root, "MaximumPowerToday", spMpptData->H21, "W", 0);
+    addInputValue(root, "MaximumPowerYesterday", spMpptData->H23, "W", 0);
 
     // power limiter state
-    root["dpl"]["PLSTATE"] = -1;
-    if (Configuration.get().PowerLimiter.Enabled)
-        root["dpl"]["PLSTATE"] = PowerLimiter.getPowerLimiterState();
+    root["dpl"]["PLSTATE"] = Configuration.get().PowerLimiter.Enabled ? PowerLimiter.getPowerLimiterState() : -1;
     root["dpl"]["PLLIMIT"] = PowerLimiter.getLastRequestedPowerLimit();
 }
 
 void WebApiWsVedirectLiveClass::onWebsocketEvent(AsyncWebSocket* server, AsyncWebSocketClient* client, AwsEventType type, void* arg, uint8_t* data, size_t len)
 {
     if (type == WS_EVT_CONNECT) {
-        char str[64];
-        snprintf(str, sizeof(str), "Websocket: [%s][%u] connect", server->url(), client->id());
-        Serial.println(str);
-        MessageOutput.println(str);
+        MessageOutput.printf("Websocket: [%s][%u] connect\r\n", server->url(), client->id());
     } else if (type == WS_EVT_DISCONNECT) {
-        char str[64];
-        snprintf(str, sizeof(str), "Websocket: [%s][%u] disconnect", server->url(), client->id());
-        Serial.println(str);
-        MessageOutput.println(str);
+        MessageOutput.printf("Websocket: [%s][%u] disconnect\r\n", server->url(), client->id());
     }
 }
 
@@ -183,7 +169,6 @@ void WebApiWsVedirectLiveClass::onLivedataStatus(AsyncWebServerRequest* request)
         return;
     }
     try {
-        std::lock_guard<std::mutex> lock(_mutex);
         AsyncJsonResponse* response = new AsyncJsonResponse(false, _responseSize);
         auto& root = response->getRoot();
 
