242e10025667df41f286bfc6b2af75f14a2331dc
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-02 23:33:45 +0100

    Update BatteryDataStatus.ts

--- webapp/src/types/BatteryDataStatus.ts
+++ webapp/src/types/BatteryDataStatus.ts
@@ -1,10 +1,13 @@
-import type { ValueObject } from '@/types/LiveDataStatus';
-
-type BatteryData = (ValueObject | string)[];
+import type { ValueObject, CellObject } from '@/types/LiveDataStatus';
 
 export interface Battery {
     manufacturer: string;
+    device_name: string;
     data_age: number;
-    values: BatteryData[];
+    software_version: string;
+    values: (ValueObject | string)[];
+    parameters: (ValueObject | string)[];
     issues: number[];
+    cell: CellObject;
+    tempSensor: ValueObject[];
 }
