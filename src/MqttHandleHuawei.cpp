b8930fec3bbeb7afa7ebafe98b7889eb7823ac6a
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-02 21:21:06 +0100

    Update MqttHandleHuawei.cpp

--- src/MqttHandleHuawei.cpp
+++ src/MqttHandleHuawei.cpp
@@ -14,6 +14,8 @@
 
 MqttHandleHuaweiClass MqttHandleHuawei;
 
+static constexpr char TAG[] = "[Huawei MQTT] ";
+
 MqttHandleHuaweiClass::MqttHandleHuaweiClass()
     : _loopTask(TASK_IMMEDIATE, TASK_FOREVER, std::bind(&MqttHandleHuaweiClass::loop, this))
 {
@@ -62,31 +64,27 @@ void MqttHandleHuaweiClass::loop()
 
     mqttLock.unlock();
 
-    if (!MqttSettings.getConnected() ) {
+    if (!MqttSettings.getConnected() || !_lastPublish.occured()) {
         return;
     }
 
-    const RectifierParameters_t *rp = HuaweiCan.get();
+    const RectifierParameters_t* rp = HuaweiCan.get();
 
-    if ((millis() - _lastPublish) > (config.Mqtt.PublishInterval * 1000) ) {
-      MqttSettings.publish("huawei/data_age", String((millis() - HuaweiCan.getLastUpdate()) / 1000));
-      MqttSettings.publish("huawei/input_voltage", String(rp->input_voltage));
-      MqttSettings.publish("huawei/input_current", String(rp->input_current));
-      MqttSettings.publish("huawei/input_power", String(rp->input_power));
-      MqttSettings.publish("huawei/output_voltage", String(rp->output_voltage));
-      MqttSettings.publish("huawei/output_current", String(rp->output_current));
-      MqttSettings.publish("huawei/max_output_current", String(rp->max_output_current));
-      MqttSettings.publish("huawei/output_power", String(rp->output_power));
-      MqttSettings.publish("huawei/input_temp", String(rp->input_temp));
-      MqttSettings.publish("huawei/output_temp", String(rp->output_temp));
-      MqttSettings.publish("huawei/efficiency", String(rp->efficiency));
+    const String subtopic = "huawei/" MqttSettings.publish(subtopic + "data_age", String((millis() - HuaweiCan.getLastUpdate()) / 1000));
+    MqttSettings.publish(subtopic + "input_voltage", String(rp->input_voltage));
+    MqttSettings.publish(subtopic + "input_current", String(rp->input_current));
+    MqttSettings.publish(subtopic + "input_power", String(rp->input_power));
+    MqttSettings.publish(subtopic + "output_voltage", String(rp->output_voltage));
+    MqttSettings.publish(subtopic + "output_current", String(rp->output_current));
+    MqttSettings.publish(subtopic + "max_output_current", String(rp->max_output_current));
+    MqttSettings.publish(subtopic + "output_power", String(rp->output_power));
+    MqttSettings.publish(subtopic + "input_temp", String(rp->input_temp));
+    MqttSettings.publish(subtopic + "output_temp", String(rp->output_temp));
+    MqttSettings.publish(subtopic + "efficiency", String(rp->efficiency));
 
-
-      yield();
-      _lastPublish = millis();
-    }
+    _lastPublish.set(Configuration.get().Mqtt.PublishInterval * 1000);
+    yield();
 }
-
 
 void MqttHandleHuaweiClass::onMqttMessage(Topic t,
         const espMqttClientTypes::MessageProperties& properties,
@@ -99,8 +97,7 @@ void MqttHandleHuaweiClass::onMqttMessage(Topic t,
         payload_val = std::stof(strValue);
     }
     catch (std::invalid_argument const& e) {
-        MessageOutput.printf("Huawei MQTT handler: cannot parse payload of topic '%s' as float: %s\r\n",
-                topic, strValue.c_str());
+        MessageOutput.printf("%s cannot parse payload of topic '%s' as float: %s\r\n", TAG, topic, strValue.c_str());
         return;
     }
 
@@ -108,25 +105,25 @@ void MqttHandleHuaweiClass::onMqttMessage(Topic t,
 
     switch (t) {
         case Topic::LimitOnlineVoltage:
-            MessageOutput.printf("Limit Voltage: %f V\r\n", payload_val);
+            MessageOutput.printf("%s Limit Voltage: %f V\r\n", TAG, payload_val);
             _mqttCallbacks.push_back(std::bind(&HuaweiCanClass::setValue,
                         &HuaweiCan, payload_val, HUAWEI_ONLINE_VOLTAGE));
             break;
 
         case Topic::LimitOfflineVoltage:
-            MessageOutput.printf("Offline Limit Voltage: %f V\r\n", payload_val);
+            MessageOutput.printf("%s Offline Limit Voltage: %f V\r\n", TAG, payload_val);
             _mqttCallbacks.push_back(std::bind(&HuaweiCanClass::setValue,
                         &HuaweiCan, payload_val, HUAWEI_OFFLINE_VOLTAGE));
             break;
 
         case Topic::LimitOnlineCurrent:
-            MessageOutput.printf("Limit Current: %f A\r\n", payload_val);
+            MessageOutput.printf("%s Limit Current: %f A\r\n", TAG, payload_val);
             _mqttCallbacks.push_back(std::bind(&HuaweiCanClass::setValue,
                         &HuaweiCan, payload_val, HUAWEI_ONLINE_CURRENT));
             break;
 
         case Topic::LimitOfflineCurrent:
-            MessageOutput.printf("Offline Limit Current: %f A\r\n", payload_val);
+            MessageOutput.printf("%s Offline Limit Current: %f A\r\n", TAG, payload_val);
             _mqttCallbacks.push_back(std::bind(&HuaweiCanClass::setValue,
                         &HuaweiCan, payload_val, HUAWEI_OFFLINE_CURRENT));
             break;
@@ -134,31 +131,31 @@ void MqttHandleHuaweiClass::onMqttMessage(Topic t,
         case Topic::Mode:
             switch (static_cast<int>(payload_val)) {
                 case 3:
-                    MessageOutput.println("[Huawei MQTT::] Received MQTT msg. New mode: Full internal control");
+                    MessageOutput.printf("%s Received MQTT msg. New mode: Full internal control\r\n", TAG);
                     _mqttCallbacks.push_back(std::bind(&HuaweiCanClass::setMode,
                                 &HuaweiCan, HUAWEI_MODE_AUTO_INT));
                     break;
 
                 case 2:
-                    MessageOutput.println("[Huawei MQTT::] Received MQTT msg. New mode: Internal on/off control, external power limit");
+                    MessageOutput.printf("%s Received MQTT msg. New mode: Internal on/off control, external power limit\r\n", TAG);
                     _mqttCallbacks.push_back(std::bind(&HuaweiCanClass::setMode,
                                 &HuaweiCan, HUAWEI_MODE_AUTO_EXT));
                     break;
 
                 case 1:
-                    MessageOutput.println("[Huawei MQTT::] Received MQTT msg. New mode: Turned ON");
+                    MessageOutput.printf("%s Received MQTT msg. New mode: Turned ON\r\n", TAG);
                     _mqttCallbacks.push_back(std::bind(&HuaweiCanClass::setMode,
                                 &HuaweiCan, HUAWEI_MODE_ON));
                     break;
 
                 case 0:
-                    MessageOutput.println("[Huawei MQTT::] Received MQTT msg. New mode: Turned OFF");
+                    MessageOutput.printf("%s Received MQTT msg. New mode: Turned OFF\r\n", TAG);
                     _mqttCallbacks.push_back(std::bind(&HuaweiCanClass::setMode,
                                 &HuaweiCan, HUAWEI_MODE_OFF));
                     break;
 
                 default:
-                    MessageOutput.printf("[Huawei MQTT::] Invalid mode %.0f\r\n", payload_val);
+                    MessageOutput.printf("%s Invalid mode %.0f\r\n", TAG, payload_val);
                     break;
             }
             break;
