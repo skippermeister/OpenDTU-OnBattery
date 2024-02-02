ccdf792734b0cfa1f1595077e14318e1db0c6949
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-02 22:45:21 +0100

    Update WebApi_ws_vedirect_live.h

--- include/WebApi_ws_vedirect_live.h
+++ include/WebApi_ws_vedirect_live.h
@@ -4,6 +4,7 @@
 #include "ArduinoJson.h"
 #include <ESPAsyncWebServer.h>
 #include <TaskSchedulerDeclarations.h>
+#include <TimeoutHelper.h>
 #include <VeDirectMpptController.h>
 #include <mutex>
 
@@ -20,7 +21,7 @@ private:
     AsyncWebServer* _server;
     AsyncWebSocket _ws;
 
-    uint32_t _lastWsPublish = 0;
+    TimeoutHelper _lastWsPublish = 0;
     uint32_t _dataAgeMillis = 0;
     static constexpr uint16_t _responseSize = 1024 + 128;
 
