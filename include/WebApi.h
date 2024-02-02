8a5c3af59964d3b409847e48baa0d28202a200d9
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-02 22:31:37 +0100

    Update WebApi.h

--- include/WebApi.h
+++ include/WebApi.h
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
+#include "ErrorMessages.h"
+#include "WebApi_REFUsol.h"
+#include "WebApi_ZeroExport.h"
 #include "WebApi_battery.h"
 #include "WebApi_config.h"
 #include "WebApi_device.h"
@@ -17,18 +20,24 @@
 #include "WebApi_network.h"
 #include "WebApi_ntp.h"
 #include "WebApi_power.h"
-#include "WebApi_powermeter.h"
 #include "WebApi_powerlimiter.h"
+#include "WebApi_powermeter.h"
 #include "WebApi_prometheus.h"
 #include "WebApi_security.h"
 #include "WebApi_sysstatus.h"
+#include "WebApi_vedirect.h"
 #include "WebApi_webapp.h"
+#include "WebApi_ws_REFUsol_live.h"
 #include "WebApi_ws_console.h"
 #include "WebApi_ws_live.h"
 #include "WebApi_ws_vedirect_live.h"
-#include "WebApi_vedirect.h"
-#include "WebApi_ws_Huawei.h"
+#ifdef CHARGER_HUAWEI
 #include "WebApi_Huawei.h"
+#include "WebApi_ws_Huawei.h"
+#else
+#include "WebApi_MeanWell.h"
+#include "WebApi_ws_MeanWell.h"
+#endif
 #include "WebApi_ws_battery.h"
 #include <ESPAsyncWebServer.h>
 #include <TaskSchedulerDeclarations.h>
@@ -65,7 +74,10 @@ private:
     WebApiPowerClass _webApiPower;
     WebApiPowerMeterClass _webApiPowerMeter;
     WebApiPowerLimiterClass _webApiPowerLimiter;
+    WebApiZeroExportClass _webApiZeroExport;
+#ifdef USE_PROMETHEUS
     WebApiPrometheusClass _webApiPrometheus;
+#endif
     WebApiSecurityClass _webApiSecurity;
     WebApiSysstatusClass _webApiSysstatus;
     WebApiWebappClass _webApiWebapp;
@@ -73,8 +85,17 @@ private:
     WebApiWsLiveClass _webApiWsLive;
     WebApiWsVedirectLiveClass _webApiWsVedirectLive;
     WebApiVedirectClass _webApiVedirect;
+#ifdef USE_REFUsol_INVERTER
+    WebApiWsREFUsolLiveClass _webApiWsREFUsolLive;
+    WebApiREFUsolClass _webApiREFUsol;
+#endif
+#ifdef CHARGER_HUAWEI
     WebApiHuaweiClass _webApiHuaweiClass;
     WebApiWsHuaweiLiveClass _webApiWsHuaweiLive;
+#else
+    WebApiMeanWellClass _webApiMeanWellClass;
+    WebApiWsMeanWellLiveClass _webApiWsMeanWellLive;
+#endif
     WebApiWsBatteryLiveClass _webApiWsBatteryLive;
 };
 
