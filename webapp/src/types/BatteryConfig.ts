de302e0396b6a318391793500d56dfeb6e05571f
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-02 23:31:34 +0100

    Update BatteryConfig.ts

--- webapp/src/types/BatteryConfig.ts
+++ webapp/src/types/BatteryConfig.ts
@@ -1,8 +1,15 @@
 export interface BatteryConfig {
     enabled: boolean;
+    pollinterval: number;
+    updatesonly: boolean;
     verbose_logging: boolean;
     provider: number;
+    can_controller_frequency: number;
     jkbms_interface: number;
     jkbms_polling_interval: number;
     mqtt_topic: string;
+    min_charge_temp: number;
+    max_charge_temp: number;
+    min_discharge_temp: number;
+    max_discharge_temp: number;
 }
