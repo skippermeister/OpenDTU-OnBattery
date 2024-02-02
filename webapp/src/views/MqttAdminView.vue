f188c473c2f909463b0570c5cac9ba4e05312888
skippermeister <146529906+skippermeister@users.noreply.github.com>
2024-02-03 00:50:58 +0100

    Update MqttAdminView.vue

--- webapp/src/views/MqttAdminView.vue
+++ webapp/src/views/MqttAdminView.vue
@@ -7,136 +7,120 @@
         <form @submit="saveMqttConfig">
             <CardElement :text="$t('mqttadmin.MqttConfiguration')" textVariant="text-bg-primary">
                 <InputElement :label="$t('mqttadmin.EnableMqtt')"
-                              v-model="mqttConfigList.mqtt_enabled"
-                              type="checkbox" wide/>
+                              v-model="mqttConfigList.enabled"
+                              type="checkbox" wide4_1/>
 
-                <InputElement v-show="mqttConfigList.mqtt_enabled"
-                              :label="$t('mqttadmin.VerboseLogging')"
-                              v-model="mqttConfigList.mqtt_verbose_logging"
-                              type="checkbox" wide/>
-
-                <InputElement v-show="mqttConfigList.mqtt_enabled"
+                <InputElement v-show="mqttConfigList.enabled"
                               :label="$t('mqttadmin.EnableHass')"
-                              v-model="mqttConfigList.mqtt_hass_enabled"
-                              type="checkbox" wide/>
+                              v-model="mqttConfigList.hass_enabled"
+                              type="checkbox" wide4_1/>
+
+                <InputElement v-show="mqttConfigList.enabled"
+                              :label="$t('mqttadmin.VerboseLogging')"
+                              v-model="mqttConfigList.verbose_logging"
+                              type="checkbox" wide4_2/>
             </CardElement>
 
             <CardElement :text="$t('mqttadmin.MqttBrokerParameter')" textVariant="text-bg-primary" add-space
-                         v-show="mqttConfigList.mqtt_enabled"
-            >
+                         v-show="mqttConfigList.enabled">
                 <InputElement :label="$t('mqttadmin.Hostname')"
-                              v-model="mqttConfigList.mqtt_hostname"
-                              type="text" maxlength="128"
+                              v-model="mqttConfigList.hostname"
+                              type="text" maxlength="128" wide3_4
                               :placeholder="$t('mqttadmin.HostnameHint')"/>
 
                 <InputElement :label="$t('mqttadmin.Port')"
-                              v-model="mqttConfigList.mqtt_port"
-                              type="number" min="1" max="65535"/>
+                              v-model="mqttConfigList.port"
+                              type="number" min="1" max="65535" wide3_2/>
 
                 <InputElement :label="$t('mqttadmin.Username')"
-                              v-model="mqttConfigList.mqtt_username"
-                              type="text" maxlength="64"
+                              v-model="mqttConfigList.username"
+                              type="text" maxlength="64" wide3_5
                               :placeholder="$t('mqttadmin.UsernameHint')"/>
 
                 <InputElement :label="$t('mqttadmin.Password')"
-                              v-model="mqttConfigList.mqtt_password"
-                              type="password" maxlength="64"
+                              v-model="mqttConfigList.password"
+                              type="password" maxlength="64" wide3_5
                               :placeholder="$t('mqttadmin.PasswordHint')"/>
 
                 <InputElement :label="$t('mqttadmin.BaseTopic')"
-                              v-model="mqttConfigList.mqtt_topic"
-                              type="text" maxlength="32"
+                              v-model="mqttConfigList.topic"
+                              type="text" maxlength="32" wide3_4
                               :placeholder="$t('mqttadmin.BaseTopicHint')"/>
 
                 <InputElement :label="$t('mqttadmin.PublishInterval')"
-                              v-model="mqttConfigList.mqtt_publish_interval"
-                              type="number" min="5" max="86400"
+                              v-model="mqttConfigList.publish_interval"
+                              type="number" min="5" max="86400" wide3_2
                               :postfix="$t('mqttadmin.Seconds')"/>
 
                 <InputElement :label="$t('mqttadmin.CleanSession')"
-                              v-model="mqttConfigList.mqtt_clean_session"
-                              type="checkbox"/>
+                              v-model="mqttConfigList.clean_session"
+                              type="checkbox" wide3_1/>
 
                 <InputElement :label="$t('mqttadmin.EnableRetain')"
-                              v-model="mqttConfigList.mqtt_retain"
-                              type="checkbox"/>
+                              v-model="mqttConfigList.retain"
+                              type="checkbox" wide3_1/>
 
                 <InputElement :label="$t('mqttadmin.EnableTls')"
-                              v-model="mqttConfigList.mqtt_tls"
-                              type="checkbox"/>
+                              v-model="mqttConfigList.tls"
+                              type="checkbox" wide3_1/>
 
-                <InputElement v-show="mqttConfigList.mqtt_tls"
+                <InputElement v-show="mqttConfigList.tls"
                               :label="$t('mqttadmin.RootCa')"
-                              v-model="mqttConfigList.mqtt_root_ca_cert"
-                              type="textarea" maxlength="2560" rows="10"/>
+                              v-model="mqttConfigList.root_ca_cert"
+                              type="textarea" maxlength="2560" rows="10" wide2_10/>
 
-                <InputElement v-show="mqttConfigList.mqtt_tls"
+                <InputElement v-show="mqttConfigList.tls"
                               :label="$t('mqttadmin.TlsCertLoginEnable')"
-                              v-model="mqttConfigList.mqtt_tls_cert_login"
-                              type="checkbox"/>
+                              v-model="mqttConfigList.tls_cert_login"
+                              type="checkbox" wide2_1/>
 
-                <InputElement v-show="mqttConfigList.mqtt_tls_cert_login"
+                <InputElement v-show="mqttConfigList.tls_cert_login"
                               :label="$t('mqttadmin.ClientCert')"
-                              v-model="mqttConfigList.mqtt_client_cert"
-                              type="textarea" maxlength="2560" rows="10"/>
+                              v-model="mqttConfigList.client_cert"
+                              type="textarea" maxlength="2560" rows="10" wide2_10/>
 
-                <InputElement v-show="mqttConfigList.mqtt_tls_cert_login"
+                <InputElement v-show="mqttConfigList.tls_cert_login"
                               :label="$t('mqttadmin.ClientKey')"
-                              v-model="mqttConfigList.mqtt_client_key"
-                              type="textarea" maxlength="2560" rows="10"/>
+                              v-model="mqttConfigList.client_key"
+                              type="textarea" maxlength="2560" rows="10" wide2_10/>
             </CardElement>
 
             <CardElement :text="$t('mqttadmin.LwtParameters')" textVariant="text-bg-primary" add-space
-                         v-show="mqttConfigList.mqtt_enabled"
-            >
+                         v-show="mqttConfigList.enabled">
                 <InputElement :label="$t('mqttadmin.LwtTopic')"
-                              v-model="mqttConfigList.mqtt_lwt_topic"
-                              type="text" maxlength="32" :prefix="mqttConfigList.mqtt_topic"
+                              v-model="mqttConfigList.lwt_topic"
+                              type="text" maxlength="32" :prefix="mqttConfigList.topic" wide3_4
                               :placeholder="$t('mqttadmin.LwtTopicHint')"/>
 
                 <InputElement :label="$t('mqttadmin.LwtOnline')"
-                              v-model="mqttConfigList.mqtt_lwt_online"
-                              type="text" maxlength="20"
+                              v-model="mqttConfigList.lwt_online"
+                              type="text" maxlength="20" wide3_4
                               :placeholder="$t('mqttadmin.LwtOnlineHint')"/>
 
                 <InputElement :label="$t('mqttadmin.LwtOffline')"
-                              v-model="mqttConfigList.mqtt_lwt_offline"
-                              type="text" maxlength="20"
+                              v-model="mqttConfigList.lwt_offline"
+                              type="text" maxlength="20" wide3_4
                               :placeholder="$t('mqttadmin.LwtOfflineHint')"/>
-
-                <div class="row mb-3">
-                    <label class="col-sm-2 col-form-label">
-                        {{ $t('mqttadmin.LwtQos') }}
-                    </label>
-                    <div class="col-sm-10">
-                        <select class="form-select" v-model="mqttConfigList.mqtt_lwt_qos">
-                            <option v-for="qostype in qosTypeList" :key="qostype.key" :value="qostype.key">
-                                {{ $t(`mqttadmin.` + qostype.value) }}
-                            </option>
-                        </select>
-                    </div>
-                </div>
             </CardElement>
 
             <CardElement :text="$t('mqttadmin.HassParameters')" textVariant="text-bg-primary" add-space
-                         v-show="mqttConfigList.mqtt_enabled && mqttConfigList.mqtt_hass_enabled"
-            >
+                         v-show="mqttConfigList.enabled && mqttConfigList.hass_enabled">
                 <InputElement :label="$t('mqttadmin.HassPrefixTopic')"
-                              v-model="mqttConfigList.mqtt_hass_topic"
-                              type="text" maxlength="32"
+                              v-model="mqttConfigList.hass_topic"
+                              type="text" maxlength="32" wide3_4
                               :placeholder="$t('mqttadmin.HassPrefixTopicHint')"/>
 
                 <InputElement :label="$t('mqttadmin.HassRetain')"
-                              v-model="mqttConfigList.mqtt_hass_retain"
-                              type="checkbox"/>
+                              v-model="mqttConfigList.hass_retain"
+                              type="checkbox" placeholder="Hass" wide3_1/>
 
                 <InputElement :label="$t('mqttadmin.HassExpire')"
-                              v-model="mqttConfigList.mqtt_hass_expire"
-                              type="checkbox"/>
+                              v-model="mqttConfigList.hass_expire"
+                              type="checkbox" wide3_1/>
 
                 <InputElement :label="$t('mqttadmin.HassIndividual')"
-                              v-model="mqttConfigList.mqtt_hass_individualpanels"
-                              type="checkbox"/>
+                              v-model="mqttConfigList.hass_individualpanels"
+                              type="checkbox" wide3_1/>
             </CardElement>
 
             <FormFooter @reload="getMqttConfig"/>
@@ -147,8 +131,8 @@
 <script lang="ts">
 import BasePage from '@/components/BasePage.vue';
 import BootstrapAlert from "@/components/BootstrapAlert.vue";
-import CardElement from '@/components/CardElement.vue';
 import FormFooter from '@/components/FormFooter.vue';
+import CardElement from '@/components/CardElement.vue';
 import InputElement from '@/components/InputElement.vue';
 import type { MqttConfig } from "@/types/MqttConfig";
 import { authHeader, handleResponse } from '@/utils/authentication';
@@ -158,8 +142,8 @@ export default defineComponent({
     components: {
         BasePage,
         BootstrapAlert,
-        CardElement,
         FormFooter,
+        CardElement,
         InputElement,
     },
     data() {
@@ -169,11 +153,6 @@ export default defineComponent({
             alertMessage: "",
             alertType: "info",
             showAlert: false,
-            qosTypeList: [
-                { key: 0, value: 'QOS0' },
-                { key: 1, value: 'QOS1' },
-                { key: 2, value: 'QOS2' },
-            ],
         };
     },
     created() {
